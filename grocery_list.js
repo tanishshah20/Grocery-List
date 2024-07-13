e455fbfa037ef25e0b0d0a3dfa54a7388522c67b
tanishshah <tanishshah20@gmail.com>
2024-07-13 17:45:09 +0530

    feature upgrade

---
+++ grocery_list.js
@@ -0,0 +1,18 @@
+const input_btn = document.getElementById("input");
+const o_list = document.getElementById("list");
+function add_to_list() {
+    if(input_btn.value !== ""){
+        const list = document.createElement("li");
+        list.innerHTML = input_btn.value;
+        o_list.appendChild(list);
+
+        list.addEventListener('click', function(){
+            this.classList.toggle('done');
+        });
+    }
+    input_btn.value = "";
+}
+list.addEventListener('click', function(){
+    this.classList.toggle('done');
+});
+
